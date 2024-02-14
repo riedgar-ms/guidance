9ec403e67d9f21b26139f1f9eabbbe4a36fa8b5d
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2024-02-14 16:04:46 -0500

    Need to download more than one file....

--- azure/scripts/hugging_face_setup.py
+++ azure/scripts/hugging_face_setup.py
@@ -1,10 +1,10 @@
-from huggingface_hub import hf_hub_download
+from huggingface_hub import snapshot_download
 
 if __name__ == "__main__":
-    path = hf_hub_download(repo_id="openai-community/gpt2", filename="config.json")
+    path = snapshot_download(repo_id="openai-community/gpt2")
     print(f"Downloaded to {path}")
-    path = hf_hub_download(repo_id="microsoft/phi-2", filename="config.json")
+    path = snapshot_download(repo_id="microsoft/phi-2")
     print(f"Downloaded to {path}")
 
     # Could also do datasets here
-    # https://huggingface.co/docs/huggingface_hub/en/guides/download#from-latest-version
+    # https://huggingface.co/docs/huggingface_hub/en/guides/download#download-an-entire-repository
