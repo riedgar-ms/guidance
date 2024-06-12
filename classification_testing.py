f3812077d00889a6e83d10b993e6f9e586d1226a
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2024-06-12 08:52:07 -0400

    Some simple test code

---
+++ classification_testing.py
@@ -0,0 +1,8 @@
+import guidance
+
+lm = guidance.models.Transformers("microsoft/Phi-3-mini-4k-instruct", trust_remote_code=True)
+
+lm += "Once upon a time there was a cat and an elephant "
+lm += guidance.gen(max_tokens=200)
+
+print(str(lm))
