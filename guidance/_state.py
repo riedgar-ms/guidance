bb3fe75ad7d0b582e295e6e25ad2f9b41b106221
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2025-08-04 10:38:20 -0400

    Starting some hacking

---
+++ guidance/_state.py
@@ -0,0 +1,34 @@
+from enum import StrEnum
+from typing import Annotated, Literal, Union
+
+from pydantic import BaseModel, Field
+
+
+class ContentType(StrEnum):
+    TEXT = "text"
+    IMAGE = "image"
+    AUDIO = "audio"
+    VIDEO = "video"
+    MULTIPLE = "multiple"
+
+
+class TextContent(BaseModel):
+    content_type: Literal[ContentType.TEXT]
+    text: str
+
+
+class ImageContent(BaseModel):
+    content_type: Literal[ContentType.IMAGE]
+    image: bytes
+    mime_type: str
+
+
+class MultipleContent(BaseModel):
+    content_type: Literal[ContentType.MULTIPLE]
+    contents: list[Union['TextContent', 'ImageContent']]
+
+
+Content = Annotated[Union[TextContent, ImageContent, MultipleContent], Field(discriminator="content_type")]
+
+
+
